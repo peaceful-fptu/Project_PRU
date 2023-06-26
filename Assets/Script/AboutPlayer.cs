1aa2dfc08e3796cf4eeb294bf129c07d64d2a636
Thanh Binh <thanhbinhdxdx@gmail.com>
2023-06-27 00:02:08 +0700

    draft

---
+++ Assets/Script/AboutPlayer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AboutPlayer : MonoBehaviour
+{
+    public static AboutPlayer _Instance;
+    public int intHealth = 100;
+    public string txtTen;
+    public int score = 0;
+    public int intEnemyKilled = 0; // so quai giet duoc
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+        if (_Instance == null)
+        {
+
+            _Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+    public void KhoiTao()
+    {
+        intHealth = 100;
+        score = 0;
+        intEnemyKilled = 0;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (intHealth <= 0)
+        {
+            intHealth = 0;
+        }
+    }
+}
---
+++ Assets/Script/LayTenPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class LayTenPlayer : MonoBehaviour
+{
+    public Text txtName;
+    public Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (AboutPlayer._Instance)
+            txtName.text = AboutPlayer._Instance.txtTen; // gan ten cho nhan vat
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = target.position; // gan vi cho bang vi tri cua target
+    }
+}
